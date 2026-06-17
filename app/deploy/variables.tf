ccdf2e09f28177b833eae5d854f7f94db5c57585
AlexYuji <61638007+AlexYuji@users.noreply.github.com>
2026-06-17 01:35:37 +0000

    chore: removing duplication between the files variables.tf and versions.tf

--- app/deploy/variables.tf
+++ app/deploy/variables.tf
@@ -1,9 +1,22 @@
-terraform {
-  required_version = "1.10.5"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.86.0"
-    }
-  }
+variable "cluster_name" {
+  description = "Nome do cluster ECS"
+  type        = string
+  default     = "app-prod-cluster"
+}
+
+variable "desired_count" {
+  description = "desired tasks"
+  type        = number
+  default     = 3
+}
+
+variable "subnets_id" {
+  description = "Subnets IDs"
+  type        = list(string)
+}
+
+variable "lb_name" {
+  description = "Load Balancer Name"
+  type        = string
+  default     = "app-prod-nlb"
 }
